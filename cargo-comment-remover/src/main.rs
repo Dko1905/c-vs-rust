02d16bf6cd76017d84f53f601a34f84e48bee35c
Daniel Florescu <Daniel.florescu1905@protonmail.ch>
2021-03-05 10:21:34 +0100

    Tried to make it faster.

--- cargo-comment-remover/src/main.rs
+++ cargo-comment-remover/src/main.rs
@@ -1,9 +1,13 @@
 use std::io::{BufRead, BufReader, BufWriter, Write};
+use std::io::{Read};
 use std::io;
 
 fn main() {
-    let fin = BufReader::new(io::stdin());
-	let mut fout = BufWriter::new(io::stdout());
+	let stdin = io::stdin();
+	let stdout = io::stdout();
+
+    let fin = BufReader::new(stdin.lock());
+	let mut fout = BufWriter::new(stdout.lock());
 
 	/* States:
 	 * 0: Normal mode
@@ -14,38 +18,38 @@ fn main() {
 	 */
 	let mut state: u32 = 0; // u32 is often faster than u64
 
-	for line in fin.lines() {
-		for ch in line.expect("Failed to line").chars() {
-			if state == 0 {
-				if ch == '/' {
-					state = 1;
-				} else {
-					write!(&mut fout, "{}", ch).expect("Failed to write");
-				}
-			} else if state == 1 {
-				if ch == '*' {
-					state = 2;
-				} else if ch == '/' {
-					state = 4;
-				} else {
-					write!(&mut fout, "{}", ch).expect("Failed to write");
-					state = 0;
-				}
-			} else if state == 2 {
-				if ch == '*' {
-					state = 3;
-				}
-			} else if state == 3 {
-				if ch == '/' {
-					state = 0;
-					continue;
-				} else {
-					state = 2;
-				}
+	for ch in fin.bytes().map(|x| x.unwrap()) {
+		if state == 0 {
+			if ch == b'/' {
+				state = 1;
+			} else {
+				write!(&mut fout, "{}", ch as char).expect("Failed to write");
 			}
-		}
-		if state == 0 || state == 4 {
-			write!(&mut fout, "\n").expect("Failed to write");
+		} else if state == 1 {
+			if ch == b'*' {
+				state = 2;
+			} else if ch == b'/' {
+				state = 4;
+			} else {
+				write!(&mut fout, "/{}", ch as char).expect("Failed to write");
+				state = 0;
+			}
+		} else if state == 2 {
+			if ch == b'*' {
+				state = 3;
+			}
+		} else if state == 3 {
+			if ch == b'/' {
+				state = 0;
+				continue;
+			} else {
+				state = 2;
+			}
+		} else if state == 4 {
+			if ch == b'\n' {
+				state = 0;
+				write!(&mut fout, "\n").expect("Failed to write");
+			}
 		}
 	}
 }
